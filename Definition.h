d967628c50463e56bd03aa2439d9c93a69eeb5cb
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2022-12-30 17:28:42 +0700

    Create file for definition purposes

---
+++ Definition.h
@@ -0,0 +1,32 @@
+#ifndef DEFINITION_H
+#define DEFINITION_H
+
+/**
+ * This file contains all the definitions used in the project.
+ * The definitions are divided into sections, e.g. pins, constants,
+ * instances, variables, etc.
+ */
+
+#include "src/model/Device.h"
+
+/*----- Relay Pins ------*/
+const uint8_t PIN_RELAY = 4;
+
+/*----- LED Pins ------*/
+const uint8_t PIN_LED = 13;
+
+/*----- Button Pins ------*/
+const uint8_t PIN_BUTTON = 2;
+
+/*----- DB Key ------*/
+const String KEY_DEVICE = "device";
+const String KEY_SCHEDULE = "schedule";
+const String KEY_SETTINGS = "settings";
+
+/*----- Instances ------*/
+// Ex: RTTP::Server server(80);
+
+/*----- Variables ------*/
+Device device;
+
+#endif
