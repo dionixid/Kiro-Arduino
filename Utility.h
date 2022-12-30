772c7b045cade200dfe6652dab28737d48f4bcba
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2022-12-30 17:29:19 +0700

    Create file for helper functions

---
+++ Utility.h
@@ -0,0 +1,13 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+/**
+ * This file contains utility functions.
+ * Utility functions are functions that are used in multiple files.
+ * Every function in this file MUST not depend on any other file,
+ * except for the Definition.h file.
+ */
+
+#include "Definition.h"
+
+#endif
