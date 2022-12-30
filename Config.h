18e7ec79b75c81b6e354c20841e3024445e95b9c
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2022-12-30 17:28:54 +0700

    Create file for configuration purposes

---
+++ Config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/**
+ * This file contains the configuration of the project.
+ * For example, the Qiro Schedule and the Setting objects
+ * are constructed in this file.
+ *
+ * DO NOT put any other functions in this file.
+ */
+
+#include "Definition.h"
+
+#endif
