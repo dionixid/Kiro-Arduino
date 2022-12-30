c259efef71053b74d711f93d68492bd573eb3276
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2022-12-30 17:29:44 +0700

    Create file for web handling purposes

---
+++ WebHandler.h
@@ -0,0 +1,14 @@
+#ifndef WEB_HANDLER_H
+#define WEB_HANDLER_H
+
+/**
+ * This file contains web related functions.
+ * For example, an implementation of the web server.
+ *
+ * DO NOT put any other functions in this file.
+ */
+
+#include "Definition.h"
+#include "Utility.h"
+
+#endif
