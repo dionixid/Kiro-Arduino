e2849e290b889f034cd6d035bd11dc827543d861
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2022-12-30 17:29:04 +0700

    Create file for display purposes

---
+++ Display.h
@@ -0,0 +1,12 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+/**
+ * This file contains display related functions.
+ * 
+ * DO NOT put any other functions in this file.
+ */
+
+#include "Definition.h"
+
+#endif
