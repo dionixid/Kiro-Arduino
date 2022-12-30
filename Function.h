77573bb72c9f2ae885ba8e8f0e02b07b4cd91f7e
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2022-12-30 17:30:03 +0700

    Create file for core functions

---
+++ Function.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+/**
+ * This file contains the core functions of the project.
+ * The core functions are the functions that dictate the functionality of the project.
+ * For example, the scheduling task should be done in this file.
+ */
+
+#include "Config.h"
+#include "Definition.h"
+#include "Display.h"
+#include "Utility.h"
+#include "WebHandler.h"
+
+#endif
