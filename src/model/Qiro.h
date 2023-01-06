fa6d016d7ee7b4835b20015abba585de011f1b7b
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-07 00:35:33 +0700

    Restructuring member field order, change `surahIds` name to `surahList`, change `surahList` type to `Surah`, and change `isValid()` method to match the base class

--- src/model/Qiro.h
+++ src/model/Qiro.h
@@ -3,21 +3,25 @@
 
 #include "../Any/Any.h"
 #include "Prayer.h"
+#include "Surah.h"
 
 struct Qiro : public Object {
     Prayer::Name name;
-    uint32_t volume;
-    uint32_t durationMinutes;
-    std::vector<uint16_t> surahIds;
+    uint16_t durationMinutes;
+    std::vector<Surah> surahList;
+    uint8_t volume;
 
     Qiro(const bool& IsValid = true)
         : m_IsValid(IsValid) {}
 
-    Qiro(Prayer::Name name, uint32_t volume, uint32_t durationMinutes, std::vector<uint16_t> surahIds)
+    Qiro(
+        const Prayer::Name& name, const uint16_t& durationMinutes, const std::vector<Surah>& surahList,
+        const uint8_t& volume
+    )
         : name(name),
-          volume(volume),
           durationMinutes(durationMinutes),
-          surahIds(surahIds),
+          surahList(surahList),
+          volume(volume),
           m_IsValid(true) {}
 
     void constructor(const std::vector<Any>& tokens) override {
@@ -26,52 +30,58 @@ struct Qiro : public Object {
             return;
         }
 
-        if (!tokens[0].isNumber() || !tokens[1].isNumber() || !tokens[2].isNumber() || !tokens[3].isArray()) {
+        if (!tokens[0].isNumber() || !tokens[1].isNumber() || !tokens[2].isArray() || !tokens[3].isNumber()) {
             m_IsValid = false;
             return;
         }
 
         name            = static_cast<Prayer::Name>(tokens[0].toInt());
-        volume          = tokens[1].toInt();
-        durationMinutes = tokens[2].toInt();
+        durationMinutes = tokens[1].toInt();
+        volume          = tokens[3].toInt();
 
         if (name < Prayer::Fajr || name > Prayer::Isha) {
             m_IsValid = false;
             return;
         }
 
-        Array list = tokens[3];
-        surahIds.clear();
+        Array array = tokens[2];
+        surahList.clear();
 
-        for (auto& value : list) {
-            if (!value.isNumber()) {
+        for (auto& e : array) {
+            if (!e.isObject()) {
                 m_IsValid = false;
                 return;
-            } else {
-                surahIds.push_back(value.toInt());
             }
+
+            Surah surah = e;
+            if (!surah) {
+                m_IsValid = false;
+                return;
+            }
+
+            surahList.push_back(surah);
         }
     }
 
     String toString() const override {
-        return stringifyMembers(static_cast<uint8_t>(name), volume, durationMinutes, surahIds);
+        return stringifyMembers(static_cast<uint8_t>(name), durationMinutes, Array::of(surahList), volume);
     }
 
     String serialize() const override {
-        return serializeMembers(static_cast<uint8_t>(name), volume, durationMinutes, surahIds);
+        return serializeMembers(static_cast<uint8_t>(name), durationMinutes, Array::of(surahList), volume);
     }
 
     bool equals(const Object& other) const override {
         const Qiro& otherQiro = static_cast<const Qiro&>(other);
-        return name == otherQiro.name && volume == otherQiro.volume && durationMinutes == otherQiro.durationMinutes
-               && surahIds == otherQiro.surahIds;
+        return name == otherQiro.name && durationMinutes == otherQiro.durationMinutes
+               && surahList == otherQiro.surahList && volume == otherQiro.volume;
     }
 
     size_t size() const override {
         return 4;
     }
 
-    bool IsValid() const override {
+    bool isValid() const override {
         return m_IsValid;
     }
 
