ef0150e70c0db02434a2a0c1687d8a1d3427a678
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-07 02:41:47 +0700

    Remove `volume` member field
    Audio volume is now declared in `Surah` class

--- src/model/Qiro.h
+++ src/model/Qiro.h
@@ -9,19 +9,16 @@ struct Qiro : public Object {
     Prayer::Name name;
     uint16_t durationMinutes;
     std::vector<Surah> surahList;
-    uint8_t volume;
 
     Qiro(const bool& IsValid = true)
         : m_IsValid(IsValid) {}
 
     Qiro(
-        const Prayer::Name& name, const uint16_t& durationMinutes, const std::vector<Surah>& surahList,
-        const uint8_t& volume
+        const Prayer::Name& name, const uint16_t& durationMinutes, const std::vector<Surah>& surahList
     )
         : name(name),
           durationMinutes(durationMinutes),
           surahList(surahList),
-          volume(volume),
           m_IsValid(true) {}
 
     void constructor(const std::vector<Any>& tokens) override {
@@ -30,14 +27,13 @@ struct Qiro : public Object {
             return;
         }
 
-        if (!tokens[0].isNumber() || !tokens[1].isNumber() || !tokens[2].isArray() || !tokens[3].isNumber()) {
+        if (!tokens[0].isNumber() || !tokens[1].isNumber() || !tokens[2].isArray()) {
             m_IsValid = false;
             return;
         }
 
         name            = static_cast<Prayer::Name>(tokens[0].toInt());
         durationMinutes = tokens[1].toInt();
-        volume          = tokens[3].toInt();
 
         if (name < Prayer::Fajr || name > Prayer::Isha) {
             m_IsValid = false;
@@ -64,21 +60,21 @@ struct Qiro : public Object {
     }
 
     String toString() const override {
-        return stringifyMembers(static_cast<uint8_t>(name), durationMinutes, Array::of(surahList), volume);
+        return stringifyMembers(static_cast<uint8_t>(name), durationMinutes, Array::of(surahList));
     }
 
     String serialize() const override {
-        return serializeMembers(static_cast<uint8_t>(name), durationMinutes, Array::of(surahList), volume);
+        return serializeMembers(static_cast<uint8_t>(name), durationMinutes, Array::of(surahList));
     }
 
     bool equals(const Object& other) const override {
         const Qiro& otherQiro = static_cast<const Qiro&>(other);
         return name == otherQiro.name && durationMinutes == otherQiro.durationMinutes
-               && surahList == otherQiro.surahList && volume == otherQiro.volume;
+               && surahList == otherQiro.surahList;
     }
 
     size_t size() const override {
-        return 4;
+        return 3;
     }
 
     bool isValid() const override {
