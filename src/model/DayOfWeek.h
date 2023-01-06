d8377c83693313b94380c92df657b9580b75e372
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-07 00:36:32 +0700

    Create `DayOfWeek` enum class

---
+++ src/model/DayOfWeek.h
@@ -0,0 +1,16 @@
+#ifndef DAY_OF_WEEK_H
+#define DAY_OF_WEEK_H
+
+#include "../Any/Any.h"
+
+enum class DayOfWeek : uint8_t {
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};
+
+#endif
