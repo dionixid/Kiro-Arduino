c1a7e7fd96406a953294600a750f17cf9d192a6e
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-07 08:37:18 +0700

    Add device credential constants

--- Definition.h
+++ Definition.h
@@ -46,6 +46,12 @@ const String KEY_SETTING_LOCATION  = F("s_location");
 const String KEY_SETTING_WIFI      = F("s_wifi");
 const String KEY_SETTING_SECURITY  = F("s_security");
 
+/*----- Device Credential ------*/
+const String DEVICE_NAME = F("Kiro");
+const String DEVICE_PASS = F("12345678");
+const String DEVICE_UUID = F("7ab16826-e32c-48b9-add6-5b72b1615a47");
+const String DEVICE_SUID = F("5A47");
+
 /*----- Instances ------*/
 // Ex: RTTP::Server server(80);
 
