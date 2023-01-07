52067c6ec17f6b703d9ef8b50c0f406ac652a49f
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-07 08:51:06 +0700

    Implement `Timer` and cleanup  unused codes

--- Kiro-Arduino.ino
+++ Kiro-Arduino.ino
@@ -2,28 +2,15 @@
 
 void setup() {
     Serial.begin(115200);
-    
-    // Test Serialize
-    Prayer prayer(Prayer::Name::Fajr, 36000, 2);
-    Serial.println(prayer.serialize());
     Log::attach(Serial, Log::Debug);
     delay(1000);
 
-    // Deserial
-    Prayer prayer2 = Any::parse("{0,36000,2}");
-    Serial.println(prayer2.serialize());
     initializeDatabase();
     initializeNetwork();
 
-    // test Validity
-    Prayer prayer3 = Any::parse("{0,36000,2}");
-    Serial.println(prayer.IsValid() ? "Valid" : "Invalid");
-    Serial.println(prayer2.IsValid() ? "Valid" : "Invalid");
-    Serial.println(prayer3.IsValid() ? "valid" : "Invalid");
     xTaskCreate(reconnectionTask, "reconnectionTask", 4096, NULL, 5, NULL);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    
+    Timer::run();
 }
