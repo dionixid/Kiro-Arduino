dca74619491a399f018a05a0587018a4702a106f
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-07 08:35:23 +0700

    Add version as macro and include dependencies

--- Definition.h
+++ Definition.h
@@ -1,12 +1,23 @@
 #ifndef DEFINITION_H
 #define DEFINITION_H
 
+#define VERSION "1.0.0"
+
 /**
  * This file contains all the definitions used in the project.
  * The definitions are divided into sections, e.g. pins, constants,
  * instances, variables, etc.
  */
 
+#include <AsyncUDP.h>
+#include <SPIFFS.h>
+#include <esp_wifi.h>
+
+#include "src/Log/Log.h"
+#include "src/RTTP/RTTP.h"
+#include "src/Time/Time.h"
+#include "src/Timer/Timer.h"
+#include "src/TinyDB/TinyDB.h"
 #include "src/model/Device.h"
 #include "src/model/Prayer.h"
 #include "src/model/PrayerGroup.h"
