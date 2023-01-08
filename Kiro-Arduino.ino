70e1dbb3e785a353b78cffe36532c40c2c62289c
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-08 18:39:55 +0700

    Initialize relay, button, and DFPlayer instances

--- Kiro-Arduino.ino
+++ Kiro-Arduino.ino
@@ -2,16 +2,26 @@
 
 void setup() {
     Serial.begin(115200);
-    Log::attach(Serial, Log::Debug);
+    Serial1.begin(9600, SERIAL_8N1, PIN_DF_RX, PIN_DF_TX);
     delay(1000);
-    Serial1.begin(9600, SERIAL_8N1, PIN_DF_TX, PIN_DF_RX);
+    Log::attach(Serial, Log::Debug);
     g_DFPlayer.begin(Serial1);
     g_OLED.begin();
     g_Button.begin();
+    g_DFBusy.begin();
+    g_Relay.begin();
+
+    g_Button.onPress(onButtonPressed);
+    g_DFBusy.onPress(onStartPlayingAudio);
+    g_DFBusy.onRelease(onFinishedPlayingAudio);
 
     initializeDatabase();
     initializeNetwork();
 
+    g_DFPlayer.setTimeOut(500);
+    g_DFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+    g_DFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+    g_DFPlayer.volume(20);
     xTaskCreate(reconnectionTask, "reconnectionTask", 4096, NULL, 5, NULL);
 }
 
