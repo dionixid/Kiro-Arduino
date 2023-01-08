3bcc8cd86b5144164b0093ec79abe588463dc838
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-08 18:25:28 +0700

    Add String constant of the group name

--- Config.h
+++ Config.h
@@ -13,6 +13,12 @@
 
 namespace Config {
 
+const String G_DATE_TIME = "Date and Time";
+const String G_LOCATION  = "Location";
+const String G_WIFI      = "WiFi";
+const String G_SECURITY  = "Security";
+const String G_ABOUT     = "About";
+
 const String TIME              = F("DT0");
 const String DATE              = F("DT1");
 const String LATITUDE          = F("L0");
@@ -28,24 +34,24 @@ void initialize() {
     g_Device.name    = DEVICE_NAME + " " + DEVICE_SUID;
     g_Device.version = VERSION;
 
-    g_DateTime.name = "Date and Time";
+    g_DateTime.name = G_DATE_TIME;
     g_DateTime.settings.push_back(Setting(TIME, Setting::Type::Time, "Time", 0));
     g_DateTime.settings.push_back(Setting(DATE, Setting::Type::Date, "Date", 0));
 
-    g_Location.name = "Location";
+    g_Location.name = G_LOCATION;
     g_Location.settings.push_back(Setting(LATITUDE, Setting::Type::Latitude, "Latitude", 0.0));
     g_Location.settings.push_back(Setting(LONGITUDE, Setting::Type::Longitude, "Longitude", 0.0));
     g_Location.settings.push_back(Setting(ELEVATION, Setting::Type::Elevation, "Elevation", 0.0));
 
-    g_WiFi.name = "WiFi";
+    g_WiFi.name = G_WIFI;
     g_WiFi.settings.push_back(Setting(WIFI_STATUS, Setting::Type::Info, "Status", "disconnected"));
     g_WiFi.settings.push_back(Setting(WIFI_SSID, Setting::Type::String, "SSID", ""));
     g_WiFi.settings.push_back(Setting(WIFI_PASSWORD, Setting::Type::String, "Password", "", true));
 
-    g_Security.name = "Security";
+    g_Security.name = G_SECURITY;
     g_Security.settings.push_back(Setting(SECURITY_PASSWORD, Setting::Type::String, "Password", DEVICE_PASS, true));
 
-    g_About.name = "About";
+    g_About.name = G_ABOUT;
     g_About.settings.push_back(Setting("A0", Setting::Type::Info, "Version", VERSION));
 
     g_QiroMonday.dayOfWeek = DayOfWeek::Monday;
