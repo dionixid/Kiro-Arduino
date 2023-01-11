784747601d89a57c0572d4cdbfd6ba354ad34657
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-11 21:37:57 +0700

    Add tag for button

--- Definition.h
+++ Definition.h
@@ -101,6 +101,7 @@ const String TAG_RTTP     = F("RTTP");
 const String TAG_UDP      = F("UDP");
 const String TAG_TIME     = F("Time");
 const String TAG_AUDIO    = F("Audio");
+const String TAG_BUTTON   = F("Button");
 
 /*----- Instances ------*/
 TinyDB g_DB;
